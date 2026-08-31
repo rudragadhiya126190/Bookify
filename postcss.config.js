d70a02d077fdf5885129e2516189248546113aa6
rudragadhiya126190 <rudra.gadhiya126190@marwadiuniversity.ac.in>
2026-08-31 22:51:45 +0530

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
-=======
-﻿Update postcss.config.js - step 89
->>>>>>> 7f70c718af881c7b811c544a95b4289299c0ab26
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-=======
-﻿Update tailwind.config.js - step 91
->>>>>>> 7f70c718af881c7b811c544a95b4289299c0ab26
+}
